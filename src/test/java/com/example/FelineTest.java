e89e6a693e8b0150443f1328e9350e291efea678
furyonee <115995660+furyonee@users.noreply.github.com>
2023-12-15 19:51:40 +0300

    Changed FeelineTest

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,38 +1,29 @@
 package com.example;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
-    @Spy
-    private Feline feline;
+    private Feline feline = new Feline();
 
     @Test
     public void getFamilyReturnsFelineValue() {
-        feline.getFamily();
         assertEquals("Кошачьи", feline.getFamily());
     }
 
     @Test
     public void getKittensWithParametersShouldReturnAppropriateValue() {
-        feline.getKittens(2);
         assertEquals(2, feline.getKittens(2));
     }
 
     @Test
     public void getKittensWithNoParametersShouldReturnOne() {
-        feline.getKittens();
         assertEquals(1, feline.getKittens());
     }
 
     @Test
     public void doesHaveMane() {
-        feline.doesHaveMane();
         assertEquals(false, feline.doesHaveMane());
     }
 }
